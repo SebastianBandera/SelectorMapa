720aa076c4c5eedfa7e475369c0aca8a3449d511
Sebastián Bandera <sebastianbandera17@gmail.com>
2024-07-28 14:56:53 -0300

    Simplicación TestMap

--- js/TestMapController.js
+++ js/TestMapController.js
@@ -1,3 +1,8 @@
+//const nominatim = 'https://nominatim.openstreetmap.org/';
+const nominatim = 'http://localhost:8080/';
+//const tile = 'https://tile.openstreetmap.org/';
+const tile = 'http://localhost:8081/tile';
+
 let mapController = new MapController({
     id_depto: 'departamento',
     id_calle: 'calle',
@@ -12,29 +17,10 @@ let mapController = new MapController({
     id_overlay: 'overlay',
     id_dir_mapa: 'dir_mapa'
 },
-'https://nominatim.openstreetmap.org/',
-'https://tile.openstreetmap.org/',
+nominatim,
+tile,
 centros.data
 );
-
-/*let mapController = new MapController({
-    id_depto: 'departamento',
-    id_calle: 'calle',
-    id_calle_numero: 'numero',
-    id_calle_esquina: 'esquina',
-    id_map: 'map',
-    id_status: 'status',
-    id_coords: 'coords',
-    id_coords_lat: 'coords_lat',
-    id_coords_lng: 'coords_lon',
-    id_toggle_todos_centros: 'toggle_todos_centros',
-    id_overlay: 'overlay',
-    id_dir_mapa: 'dir_mapa'
-},
-'http://localhost:8080/',
-'http://localhost:8081/tile',
-centros.data
-);*/
 
 mapController.startControl();
 
